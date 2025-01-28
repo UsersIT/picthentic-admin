fc2987dd2394e26ce41a6814e183c535cc811f5d
Marina Diubailo <marinadiubailo@gmail.com>
2025-01-28 14:11:41 +0300

    IN-73 chore: add sidebar icons

---
+++ src/shared/assets/icons/components/Person.tsx
@@ -0,0 +1,27 @@
+import { Ref, SVGProps, forwardRef, memo } from 'react'
+
+const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg
+    fill={'none'}
+    height={24}
+    ref={ref}
+    width={24}
+    xmlns={'http://www.w3.org/2000/svg'}
+    {...props}
+  >
+    <g clipPath={'url(#a)'} fill={'currentColor'}>
+      <path
+        d={
+          'M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM18 21a1 1 0 0 0 1-1 7 7 0 1 0-14 0 1 1 0 0 0 1 1h12Z'
+        }
+      />
+    </g>
+    <defs>
+      <clipPath id={'a'}>
+        <path d={'M0 0h24v24H0z'} fill={'#fff'} />
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export const Person = memo(forwardRef(SvgComponent))
